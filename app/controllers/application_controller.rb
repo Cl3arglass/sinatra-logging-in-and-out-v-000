74a77fd0738ef533010bd2c891c185c4b9984f48
Holden Altaffer <haltaffer@gmail.com>
2018-06-21 19:39:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,12 +11,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
+    @user = Helpers.current_user(session)
    if @user
      session[:user_id] = @user.id
      redirect '/account'
    end
-   redirect '/'
+   erb :error
   end
 
   get '/account' do
