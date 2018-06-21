e64292def2307762605c0f749bf304e4b72c6c0e
Holden Altaffer <haltaffer@gmail.com>
2018-06-21 19:20:28 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers
   def self.current_user(session)
-    @user = User.find_by(username: session[:username])
-    @user
+    # @user = User.find_by(username: session[:username])
+    # @user
+    puts session
   end
 end
